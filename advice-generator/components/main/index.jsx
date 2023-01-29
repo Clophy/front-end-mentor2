e15409f2c46173a0d6f49e898249390d5693ba61
Clophy <barangenez@hotmail.com>
2023-01-29 19:31:23 +0300

    Update index.jsx

--- advice-generator/components/main/index.jsx
+++ advice-generator/components/main/index.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useEffect,useState} from "react";
+import React, {useState} from "react";
 import { Manrope } from "@next/font/google";
 import Image from "next/image";
 
